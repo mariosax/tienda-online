7c75df80f93b63da273f12a9edf316a7fe5cc76d
mariosax <mario.garcia.lopez@hotmail.com>
2025-01-20 13:34:20 -0600

    Textos Productos

--- src/data/products.js
+++ src/data/products.js
@@ -1,17 +1,17 @@
 
 export const productsData = [
   { id: 1, name: 'Blusa Amarilla', price: 499, description: 'Blusa de algodón ', category: 'Categoría 1', image: '/assets/BlusaAmarilla.jpg' },
-  { id: 2, name: 'Producto 2', price: 200, description: 'Descripción del Producto 2', category: 'Categoría 2', image: '/assets/ZapatosHombre.jpg'},
-  { id: 3, name: 'Producto 3', price: 300, description: 'Descripción del Producto 3', category: 'Categoría 1', image: '/assets/ChamarraAzul.jpg' },
-  { id: 4, name: 'Producto 3', price: 300, description: 'Descripción del Producto 3', category: 'Categoría 1', image: '/assets/Guantes.jpg' },
-  { id: 5, name: 'Producto 3', price: 300, description: 'Descripción del Producto 3', category: 'Categoría 1', image: '/assets/Sueter.jpg' },
-  { id: 6, name: 'Producto 3', price: 300, description: 'Descripción del Producto 3', category: 'Categoría 1', image: '/assets/Camisa.jpg' },
-  { id: 7, name: 'Producto 3', price: 300, description: 'Descripción del Producto 3', category: 'Categoría 1', image: '/assets/Playera.jpg' },
-  { id: 8, name: 'Producto 3', price: 300, description: 'Descripción del Producto 3', category: 'Categoría 1', image: '/assets/Maleta.jpg' },
-  { id: 9, name: 'Producto 3', price: 300, description: 'Descripción del Producto 3', category: 'Categoría 1', image: '/assets/Abrigo.jpg' },
-  { id: 10, name: 'Producto 3', price: 300, description: 'Descripción del Producto 3', category: 'Categoría 1', image: '/assets/Cesto.jpg' },
-  { id: 11, name: 'Producto 3', price: 300, description: 'Descripción del Producto 3', category: 'Categoría 1', image: '/assets/Mochila.jpg' },
-  { id: 12, name: 'Producto 3', price: 300, description: 'Descripción del Producto 3', category: 'Categoría 1', image: '/assets/BlusaOtono.jpg' },
+  { id: 2, name: 'Zapatos miel', price: 200, description: 'Zapatos de uso rudo', category: 'Categoría 2', image: '/assets/ZapatosHombre.jpg'},
+  { id: 3, name: 'Chamarra Mezclilla', price: 300, description: 'Chamarra aborregada', category: 'Categoría 1', image: '/assets/ChamarraAzul.jpg' },
+  { id: 4, name: 'Guantes', price: 300, description: 'Invernales', category: 'Categoría 1', image: '/assets/Guantes.jpg' },
+  { id: 5, name: 'Sueter', price: 300, description: 'Sueter polar', category: 'Categoría 1', image: '/assets/Sueter.jpg' },
+  { id: 6, name: 'Camisa', price: 300, description: 'Camisa de poliester', category: 'Categoría 1', image: '/assets/Camisa.jpg' },
+  { id: 7, name: 'Camisa rosa', price: 300, description: 'Playera americana tipo polo', category: 'Categoría 1', image: '/assets/Playera.jpg' },
+  { id: 8, name: 'Maleta', price: 300, description: 'Maleta 20 kg', category: 'Categoría 1', image: '/assets/Maleta.jpg' },
+  { id: 9, name: 'Chamarra', price: 300, description: 'Abrigo polar', category: 'Categoría 1', image: '/assets/Abrigo.jpg' },
+  { id: 10, name: 'Cesto', price: 300, description: 'Cesto ropa 20kg', category: 'Categoría 1', image: '/assets/Cesto.jpg' },
+  { id: 11, name: 'Mochila', price: 300, description: 'Mochila tipo Inti', category: 'Categoría 1', image: '/assets/Mochila.jpg' },
+  { id: 12, name: 'Blusa Otoño', price: 300, description: 'Descripción del Producto 3', category: 'Categoría 1', image: '/assets/BlusaOtono.jpg' },
  
   // Otros productos...
 ];
